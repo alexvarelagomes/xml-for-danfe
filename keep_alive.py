06c9dcf06b5693f0b78c508613690075fbe06450
Alex <alex-fjp@hotmail.com>
2026-05-19 20:58:46 -0300

    ✨ feat: Adiciona script de reativação da aplicação utilizando Playwright e atualiza o workflow keep_alive.yml

---
+++ keep_alive.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import sync_playwright
+import time
+
+def manter_ativo():
+    with sync_playwright() as p:
+
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto("https://xml-for-danfe.streamlit.app/", wait_until="networkidle")     
+
+        botao_restaurar = page.get_by_text("Sim, vamos restaurar esse aplicativo!")
+        
+        if botao_restaurar.is_visible():
+            print("Aplicação em repouso detectada. Clicando no botão para reativar...")
+            botao_restaurar.click()
+            time.sleep(15) 
+            print("Processo de reativação concluído.")
+        else:
+            print("A aplicação já está ativa e rodando normalmente. Nenhum clique necessário.")
+            
+        browser.close()
+
+if __name__ == "__main__":
+    manter_ativo()
